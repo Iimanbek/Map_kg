0d147bc32966548f9bdc466771e44fb6f1451dba
Iimanbek <imanbek.akylbekov2017@gmail.com>
2023-03-10 11:37:35 +0600

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// // https://nuxt.com/docs/api/configuration/nuxt-config
+// import { defineNuxtConfig } from "nuxt/config"
+// export default defineNuxtConfig({
+//     // modules: ['@nuxtjs/tailwindcss']
+//     build: {
+//         postcss: {
+//           postcssOptions: {
+//             plugins: {
+//               tailwindcss: {},
+//               autoprefixer: {},
+//             },
+//           },
+//         },
+//       },
+//       css: [
+//         "@/assets/css/tailwind"
+//        ]
+// })
+// import { defineNuxtConfig } from '#imports'
+
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss']
+    modules: [
+        '@nuxtjs/tailwindcss'
+    ]
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <h1>Main page</h1>
+        <h1 class="text-6xl font-bold underline ">
+        Hello world!
+        </h1>
     </div>
 </template>
 <script>
