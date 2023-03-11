daea2c07c28758e2597e5909252ab0fbfd33492a
unknown <abudzanboltaev@gmail.com>
2023-03-12 05:46:41 +0600

    first-commit

--- components/Cotent.vue
+++ components/Cotent.vue
@@ -1,33 +1,33 @@
 <template>
-    <div class="mt-[250px]">
-        <div>
-
+    <div class="mt-[250px] bg-[rgba(0,0,0,1)] z-20 rounded-t-[15px] ">
+        <div class="flex justify-center items-center">
+            <img class="opacity-[0.6] rounded-t-[15px] " src="../assets/image/bishkek.jpg">
+            <p class="absolute text-white text-[25px]">Bishkek</p>
         </div>
-        <div>
-
+        <div class="flex justify-center items-center">
+            <img class="w-[100%] opacity-[0.6] " src="../assets/image/isyk-kol.jpeg" alt="">
+            <p class="absolute text-white text-[25px]">Isyk-Kol</p>
         </div>
-        <div>
-
+        <div class="flex justify-center items-center">
+            <img class="opacity-[0.6]" src="../assets/image/kara-kol.jpg" alt="">
+            <p class="absolute text-white text-[25px]">Kara-Kol</p>
         </div>
-        <div>
-
+        <div class="flex justify-center items-center">
+            <img class="opacity-[0.6]" src="../assets/image/talas.jpg" alt="">
+            <p class="absolute text-white text-[25px]">Talas</p>
         </div>
-        <div>
-
+        <div class="flex justify-center items-center">
+            <img class="w-[100%] opacity-[0.6] " src="../assets/image/batken.jpeg"  alt="" >
+            <p class="absolute text-white text-[25px]">Batken</p>
         </div>
-        <div>
-
+        <div class="flex justify-center items-center">
+            <img class="w-[100%] opacity-[0.6] " src="../assets/image/osh.jpeg" alt="">
+            <p class="absolute text-white text-[25px]">Osh</p>
         </div>
-        <div>
-
+        <div class="flex justify-center items-center">
+            <img class="opacity-[0.6]" src="../assets/image/narin.jpg" alt="">
+            <p class="absolute text-white text-[25px]">Naryn</p>
         </div>
-        <img class="  " src="../assets/image/bishkek.jpg">
-        <img class="w-[100%]  " src="../assets/image/isyk-kol.jpeg" alt="">
-        <img src="../assets/image/kara-kol.jpg" alt="">
-        <img src="../assets/image/talas.jpg" alt="">
-        <img class="w-[100%]  " src="../assets/image/batken.jpeg"  alt="" >
-        <img class="w-[100%]  " src="../assets/image/osh.jpeg" alt="">
-        <img src="../assets/image/narin.jpg" alt="">
     </div>
 </template>
 <script>
