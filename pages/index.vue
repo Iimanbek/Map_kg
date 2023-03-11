a85798b9fd6da1dec1e3e3556341c7c30e8fcae5
Iimanbek <imanbek.akylbekov2017@gmail.com>
2023-03-11 20:55:21 +0600

    add

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,26 @@
 <template>
     <div>
-		<NuxtLayout>
-			<div class = "grid pt-[80px]">
-				<div class = "flex flex-col gap-4 justify-self-center [&_*]:border-2 [&_*]:border-red-500 [&_*]:p-2">
-					<input placeholder = "Введите Ваше местоположение"/>
-					<input placeholder = "Введите куда Вы направляетесь"/>
-				</div>
-				<button @click = "showMap()" class = "flex justify-self-center bg-orange-500 rounded-sm text-white m-4 p-2">Найти маршрут</button>
-			</div>
-			<div>
-				<img :src = "map" />
-			</div>
-		</NuxtLayout>
+        <NuxtLayout>
+            <div class="grid pt-[80px]">
+                <div class="flex flex-col gap-4 justify-self-center [&_*]:border-2 [&_*]:border-red-500 [&_*]:p-2">
+                    <input placeholder="Введите Ваше местоположение" />
+                    <input placeholder="Введите куда Вы направляетесь" />
+                </div>
+                <button @click="showMap()"
+                    class="flex justify-self-center bg-orange-500 rounded-sm text-white m-4 p-2">Найти маршрут</button>
+            </div>
+            <div>
+
+                <img :src="map" />
+            </div>
+        </NuxtLayout>
     </div>
 </template>
 <script setup lang="ts">
 
 let h: string = "Hello World!";
 let clickFlip: boolean;
-let map: Ref<string> = ref(''); 
+let map: Ref<string> = ref('');
 console.log(typeof map)
 let test: number = 4;
 test = 'hghg';
@@ -26,8 +28,8 @@ console.log(test);
 
 function showMap() {
 
-	map.value = 'easy.gif';
-	console.log(map);
+    map.value = 'easy.gif';
+    console.log(map);
 
 }
 
