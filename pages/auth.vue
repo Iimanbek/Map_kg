3e0d7828119cb88f43a678d7b23c0de9f8aba0fc
Iimanbek <imanbek.akylbekov2017@gmail.com>
2023-03-12 04:53:39 +0600

    add

--- pages/auth.vue
+++ pages/auth.vue
@@ -46,7 +46,7 @@
                                     class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
                                     password?</a>
                             </div>
-                            <button @click="testing" type="submit"
+                            <button @click="this.$router.push('/user')" type="submit"
                                 class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign
                                 in</button>
                             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
