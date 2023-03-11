ff30e6ead1da6138553ca81b280fe5b2474e7de9
Iimanbek <imanbek.akylbekov2017@gmail.com>
2023-03-11 22:16:22 +0600

    add some cards

--- pages/index.vue
+++ pages/index.vue
@@ -9,9 +9,25 @@
                 <button @click="showMap()"
                     class="flex justify-self-center bg-orange-500 rounded-sm text-white m-4 p-2">Найти маршрут</button>
             </div>
+            <div v-if="map">
+                <img src="easy.gif" />
+            </div>
+            <div v-if="!map">
+                <div style="position:relative;overflow:hidden;"><a
+                        href="https://yandex.ru/maps/10309/bishkek/?utm_medium=mapframe&utm_source=maps"
+                        style="color:#eee;font-size:12px;position:absolute;top:0px;">Бишкек</a><a
+                        href="https://yandex.ru/maps/geo/771290125/?ll=74.586871%2C42.899532&rtext=55.909968%2C37.736743~55.743692%2C37.567571&rtn=2&rtt=mt&ruri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1MzA2MzU1OBI_0KDQvtGB0YHQuNGPLCDQnNC-0YHQutC-0LLRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwg0JzRi9GC0LjRidC4IgoNbfIWQhXOo19C~ymapsbm1%3A%2F%2Ftransit%2Fstop%3Fid%3Dstation__9858821&utm_medium=mapframe&utm_source=maps&z=11.58"
+                        style="color:#eee;font-size:12px;position:absolute;top:14px;">Бишкек — Яндекс Карты</a><iframe
+                        src="https://yandex.ru/map-widget/v1/?ll=74.586871%2C42.899532&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgk3NzEyOTAxMjUSItCa0YvRgNCz0YvQt9GB0YLQsNC9LCDQkdC40YjQutC10LoiCg0YNZVCFf6AK0I%2C&rtext=55.909968%2C37.736743~55.743692%2C37.567571&rtn=2&rtt=mt&ruri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1MzA2MzU1OBI_0KDQvtGB0YHQuNGPLCDQnNC-0YHQutC-0LLRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwg0JzRi9GC0LjRidC4IgoNbfIWQhXOo19C~ymapsbm1%3A%2F%2Ftransit%2Fstop%3Fid%3Dstation__9858821&z=11.58"
+                        width="100%" height="400" frameborder="1" allowfullscreen="true"
+                        style="position:relative;"></iframe>
+                </div>
+            </div>
             <div>
 
-                <img :src="map" />
+            </div>
+            <div>
+                <card />
             </div>
         </NuxtLayout>
     </div>
@@ -20,15 +36,12 @@
 
 let h: string = "Hello World!";
 let clickFlip: boolean;
-let map: Ref<string> = ref('');
+let map: Ref<boolean> = ref(false);
 console.log(typeof map)
-let test: number = 4;
-test = 'hghg';
-console.log(test);
 
 function showMap() {
 
-    map.value = 'easy.gif';
+    map.value = !map.value;
     console.log(map);
 
 }
